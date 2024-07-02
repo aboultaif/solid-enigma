896637ca46f1e3bad9551aef6600f1f90f28111b
codecademy <ccuser@codecademy.com>
2024-07-02 08:17:16 +0000

    first commit

---
+++ tourist_attractions/forms.py
@@ -0,0 +1,12 @@
+from .models import State, Attraction 
+from django import forms
+
+class StateCreateForm(forms.ModelForm):
+  class Meta:
+    model = State
+    fields = "__all__"
+
+class AttractionCreateForm(forms.ModelForm):
+    class Meta:
+      model = Attraction 
+      fields = "__all__"
